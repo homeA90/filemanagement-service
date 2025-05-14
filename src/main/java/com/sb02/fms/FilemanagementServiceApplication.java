2cdc0bc959959d4324a45efdda2f7f964bb1ea24
hagyutae <gted221@gmail.com>
2025-05-14 16:46:13 +0900

    first commit

---
+++ src/main/java/com/sb02/fms/FilemanagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sb02.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilemanagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sb02/fms/FilemanagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb02.fms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemanagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
